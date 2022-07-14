59dccbb3cdd9c45a604f5c22f6d1fa1bf350f8b0
zgyym <2064635072@qq.com>
2022-07-14 22:25:23 +0800

    commit

--- SpringMVC-demo01/src/main/java/com/yanmo/mvc/controller/HelloController.java
+++ SpringMVC-demo01/src/main/java/com/yanmo/mvc/controller/HelloController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-
+    // @RequestMapping注解：处理请求和控制器方法之间的映射关系
+    // @RequestMapping注解的value属性可以通过请求地址匹配请求，/表示的当前工程的上下文路径
+    // localhost:8080/springMVC/
     //根据请求地址的url“/” 跳转到 /WEB-INF/templates/index.html
     @RequestMapping(value = "/")
     public String index(){
