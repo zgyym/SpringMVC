7ad5dd52a9a607a9d5f1ddb554c6337b5949acd5
zgyym <2064635072@qq.com>
2022-07-18 23:17:17 +0800

    commit

--- SpringMVC-demo02/src/main/java/com/yanmo/mvc/controller/ParamController.java
+++ SpringMVC-demo02/src/main/java/com/yanmo/mvc/controller/ParamController.java
@@ -1,7 +1,10 @@
 package com.yanmo.mvc.controller;
 
 
+import com.yanmo.mvc.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,11 +33,21 @@ public class ParamController {
     public String testParam(
             @RequestParam("username") String username,
             @RequestParam(value = "password",defaultValue = "123123")String password,
-            @RequestParam(value = "hobby",required = false)String[] hobby) {
+            @RequestParam(value = "hobby",required = false)String[] hobby,
+            @RequestHeader("host") String  host) {
+
         System.out.println(username);
         System.out.println(password);
         String s = Arrays.toString(hobby);
         System.out.println(s);
+        System.out.println(host);
+
+        return "success";
+    }
+
+    @RequestMapping("/testBean")
+    public String testBean(User user){
+        System.out.println(user);
         return "success";
     }
 }
